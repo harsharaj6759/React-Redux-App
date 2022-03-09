f60846b7667d9a8af4231347a05c5f93823ebdf1
harsharaj6759 <61135601+harsharaj6759@users.noreply.github.com>
2022-03-10 03:04:07 +0530

    Header

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+function Home() {
+  return <Container> Home </Container>;
+}
+
+export default Home;
+
+const Container = styled.div``;
