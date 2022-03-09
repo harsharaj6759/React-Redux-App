44e4304f29e4e4422ed20e3bf439d2848f1aa358
harsharaj6759 <61135601+harsharaj6759@users.noreply.github.com>
2022-03-10 03:55:57 +0530

    Image Slider

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Styled from "styled-components";
+import Slider from "react-slick";
+
+function ImgSlider() {
+  return <div> Image Slider </div>;
+}
+
+export default ImgSlider;
